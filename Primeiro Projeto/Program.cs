5f757b4a726a84d4be48d2aa4eb0b2eca77e8e66
José Borges <jose.carlos05062000@gmail.com>
2025-05-21 21:20:04 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro Projeto/Program.cs
@@ -0,0 +1,78 @@
+﻿// Screen Sound
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+List<string> Bandas = new List<string> { "U2", "The Beatles", "Nirvana"};
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+
+}
+
+void ExibirMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\n[1] - Registrar uma banda");
+    Console.WriteLine("[2] - Mostrar todas as bandas");
+    Console.WriteLine("[3] - Avaliar uma banda");
+    Console.WriteLine("[4] - Exibir a média de uma banda");
+    Console.WriteLine("[0] - Sair");
+
+    Console.Write("\nDigite a opção desejada: ");
+    string opcaoEscolhida =  Console.ReadLine()!;
+    
+    switch (int.Parse(opcaoEscolhida))
+    {
+        case 1: RegistrarBanda(); break;
+        case 2: ExibirBandas(); break;
+        case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida); break;
+        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida); break;
+        case 0: Console.WriteLine("Finalizando programa. Bye! "); break;
+        default: Console.WriteLine("Opção inválida!!"); break;
+    }
+}
+
+void RegistrarBanda()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de bandas");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    Bandas.Add(nomeDaBanda);
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirMenu();
+}
+
+void ExibirBandas()
+{
+    Console.Clear();
+    Console.WriteLine("Exibindo todas as bandas registradas");
+    int i = 1;
+    //for (int i = 0; i < Bandas.Count; i++)
+    //{
+    //    Console.WriteLine($"[{i + 1}] - {Bandas[i]}");
+    //}
+
+    foreach (string Banda in Bandas)
+    {
+        Console.WriteLine($"[{i}] - {Banda}");
+        i++;
+    }
+    Console.WriteLine("\nPressione qualquer tecla para retornar ao Menu");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirMenu();
+
+}
+
+ExibirMenu();
